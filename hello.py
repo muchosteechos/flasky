c21268bac8ac732ce5a02edf7e44381712f16613
Steen Sziegaud <s.sziegaud@gmail.com>
2019-06-02 18:35:58 +0200

    Use flash messages to indicate unusual form data submissions of differing names

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
@@ -20,6 +20,9 @@ moment = Moment(app)
 def index():
     form = NameForm()
     if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
         session['name'] = form.name.data
         return redirect(url_for('index'))
     return render_template(
